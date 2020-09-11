e93ea30f53d813cbe524d50b85ca9607348f03a7
pratikinamake <36621212+pratikinamake@users.noreply.github.com>
2020-09-11 10:17:14 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
 
 sudo yum -y update
-sudo yum -y install httpd
-sudo yum -y install nmap
-
-sudo yum -y install mysql-server libapache2-mod-auth-mysql php5-mysql 
-sudo yum -y install install php5 libapache2-mod-php5 php5-mcrypt
+sudo yum -y remove httpd
