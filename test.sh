3d10fa9d15ec3270357a2d8e46fb10f7e734d35e
root <root@ip-172-31-3-236.ap-south-1.compute.internal>
2020-09-11 04:54:17 +0000

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 
 sudo yum -y update
 sudo yum -y install httpd
+cp /root/final/index.php  /var/www/html
+sudo service httpd restart
 sudo yum -y install nmap
 
 sudo yum -y install mysql-server libapache2-mod-auth-mysql php5-mysql 
