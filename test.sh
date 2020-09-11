b1d9dce8e85ba2441ebab5f85a666222142d58dd
root <root@ip-172-31-3-236.ap-south-1.compute.internal>
2020-09-11 04:39:55 +0000

    scriptforlamp

---
+++ test.sh
@@ -0,0 +1,7 @@
+
+yum -y update
+yum -y install httpd
+yum -y install nmap
+
+yum -y install mysql-server libapache2-mod-auth-mysql php5-mysql 
+yum -y install install php5 libapache2-mod-php5 php5-mcrypt
