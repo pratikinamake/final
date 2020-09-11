853c1a93fb596e2edcf3464cbd8f28d2760bd8fb
root <root@ip-172-31-3-236.ap-south-1.compute.internal>
2020-09-11 04:45:22 +0000

    correction

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 
-yum -y update
-yum -y install httpd
-yum -y install nmap
+sudo yum -y update
+sudo yum -y install httpd
+sudo yum -y install nmap
 
-yum -y install mysql-server libapache2-mod-auth-mysql php5-mysql 
-yum -y install install php5 libapache2-mod-php5 php5-mcrypt
+sudo yum -y install mysql-server libapache2-mod-auth-mysql php5-mysql 
+sudo yum -y install install php5 libapache2-mod-php5 php5-mcrypt
